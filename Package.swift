f68f69117c3fdd232934e48d3402452a7fc15488
Ilia Biltuev <ilia.biltuev@ukg.com>
2023-05-08 18:49:09 +0300

    added binary target RealmSwift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MySPMPackage",
-            targets: ["MySPMPackage"]),
+            targets: ["RealmSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MySPMPackage",
-            dependencies: []),
-        .testTarget(
-            name: "MySPMPackageTests",
-            dependencies: ["MySPMPackage"]),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/blob/main/RealmSwift.xcframework.zip",
+            checksum: "0f4ad887b94288a6b60dda580217090f8765beb13ad761b16700c5db6e601fc8")
     ]
 )
