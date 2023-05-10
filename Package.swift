ea1ea1f2113ccd5f001848b0462908034ae86cb4
Ilia Biltuev <ilia.biltuev@ukg.com>
2023-05-10 17:45:43 +0300

    added kingfisher

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MySPMPackage",
-            targets: ["RealmSwift"]),
+            targets: ["Kingfisher"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "RealmSwift",
-            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.0/RealmSwift.xcframework.zip",
-            checksum: "0f4ad887b94288a6b60dda580217090f8765beb13ad761b16700c5db6e601fc8")
+            name: "Kingfisher",
+            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/Kingfisher.xcframework.zip",
+            checksum: "b96414ae8dddbf79f7bf22f40af667d9ff848ad78e3ee50309604975f0345f32")
     ]
 )
