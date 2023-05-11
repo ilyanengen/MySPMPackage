febd951e32eab9dc0e1e0953bc479cc71c11a041
Ilia Biltuev <ilia.biltuev@ukg.com>
2023-05-11 14:35:25 +0300

    added Starscream

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MySPMPackage",
-            targets: ["Kingfisher"]),
+            targets: ["Kingfisher", "Starscream"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -17,6 +17,10 @@ let package = Package(
         .binaryTarget(
             name: "Kingfisher",
             url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/Kingfisher.xcframework.zip",
-            checksum: "b96414ae8dddbf79f7bf22f40af667d9ff848ad78e3ee50309604975f0345f32")
+            checksum: "b96414ae8dddbf79f7bf22f40af667d9ff848ad78e3ee50309604975f0345f32"),
+        .binaryTarget(
+            name: "Starscream",
+            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/Starscream.xcframework.zip",
+            checksum: "db83ff647c764d0945bd69c3da322d19aeb6cd45da20d5d25ce0c8a53c602fa4")
     ]
 )
