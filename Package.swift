ca1fef5f0ce51e2e5c10d43095e83033ae951342
Ilia Biltuev <ilia.biltuev@ukg.com>
2023-05-11 20:38:51 +0300

    added SwiftyJSON and BSImagePicker

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "MySPMPackage",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MySPMPackage",
-            targets: ["Kingfisher", "Starscream"]),
+            targets: ["Kingfisher", "Starscream", "SwiftyJSON", "BSImagePicker"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Kingfisher",
             url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/Kingfisher.xcframework.zip",
@@ -21,6 +18,14 @@ let package = Package(
         .binaryTarget(
             name: "Starscream",
             url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/Starscream.xcframework.zip",
-            checksum: "db83ff647c764d0945bd69c3da322d19aeb6cd45da20d5d25ce0c8a53c602fa4")
+            checksum: "db83ff647c764d0945bd69c3da322d19aeb6cd45da20d5d25ce0c8a53c602fa4"),
+        .binaryTarget(
+            name: "SwiftyJSON",
+            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/SwiftyJSON.xcframework.zip",
+            checksum: "17593738b53d02ddf097e6fb7291aba278f8904e2fe23e4739b2bce4657b0e57"),
+        .binaryTarget(
+            name: "BSImagePicker",
+            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/BSImagePicker.xcframework.zip",
+            checksum: "0c7257eee0c99b03926321614c8716e88e871349f15145e9cccf60dd183ada42")
     ]
 )
