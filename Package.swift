9069fbb10a225037af2c756e26c2999f7d373964
Ilia Biltuev <ilia.biltuev@ukg.com>
2023-05-15 14:03:45 +0300

    minor fix

--- Package.swift
+++ Package.swift
@@ -28,16 +28,16 @@ let package = Package(
             url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/BSImagePicker.xcframework.zip",
             checksum: "0c7257eee0c99b03926321614c8716e88e871349f15145e9cccf60dd183ada42"),
         .binaryTarget(
-            name: "PryntTrimmerView",
-            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/PryntTrimmerView.xcframework.zip",
-            checksum: "639e2cff778c963e4c183621437ad148fde9f18c0843c28885eb8aa82f62f2d9"),
-        .binaryTarget(
             name: "Reachability",
             url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/Reachability.xcframework.zip",
             checksum: "c4af966b1196d71395533488a8f095492b947a162c7b97a9c6ee891b1a7286e9"),
         .binaryTarget(
             name: "Promises",
             url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/Promises.xcframework.zip",
-            checksum: "eb9bdac3d2da213fc63e91cea034ebe8eb41eea0254fdaecf7a4908d4c46091d")
+            checksum: "eb9bdac3d2da213fc63e91cea034ebe8eb41eea0254fdaecf7a4908d4c46091d"),
+        .binaryTarget(
+            name: "PryntTrimmerView",
+            url: "https://github.com/ilyanengen/MyBinaryXCFrameworks/releases/download/1.0.1/PryntTrimmerView.xcframework.zip",
+            checksum: "639e2cff778c963e4c183621437ad148fde9f18c0843c28885eb8aa82f62f2d9")
     ]
 )
